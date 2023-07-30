ca0261b941c68c383574d38f339e06950735ee03
Phuoc Quach ngoc <phuocquach.nt@gmail.com>
2023-07-30 23:33:57 +0700

    Initial Commit

---
+++ Landmarks/LandmarksApp.swift
@@ -0,0 +1,17 @@
+//
+//  LandmarksApp.swift
+//  Landmarks
+//
+//  Created by Phuoc Quach on 30/07/2023.
+//
+
+import SwiftUI
+
+@main
+struct LandmarksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
